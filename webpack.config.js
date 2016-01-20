c50af51a03bebd4429eb40a15118f80b45924013
coderwin <792041894@qq.com>
2016-01-20 14:29:19 +0800

    update me

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 var webpack = require('webpack');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+//var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var env = process.argv.splice(2);
 //env[0] 入口
 //env[1] 出口
@@ -8,32 +8,32 @@ module.exports = {
 	output: {
 		path: __dirname + "/build/",
 		filename: "test.js"
-	},
+	}
 	// 新添加的module属性
-    module: {
-        /*preLoaders: [{
+    /*module: {
+        preLoaders: [{
             test: /\.js$/,
             exclude: /node_modules/,
             loader: 'jsxhint'
-        }],*/
+        }],
         loaders: [
-            // {test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
+            {test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
             {test: /\.jsx$/, loader: 'jsx-loader' },
             {test: /\.css$/, loader: "style!css" },
-           /* {test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")},*/
-            {test: /\.(jpg|png)$/, loader: 'url-loader'},/*?limit=8192*/
+			{test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")},
+            {test: /\.(jpg|png)$/, loader: 'url-loader'},//?limit=8192
             {test: /\.scss$/, loader: "style!css!sass"}
         ]
-    }/*,
+    },
 	plugins: [
 	    new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
             }
         })
-	]*/
+	]
 
     ,plugins: [
         new ExtractTextPlugin("./[name].css", {allChunks: true})
-    ]
+    ]*/
 };
